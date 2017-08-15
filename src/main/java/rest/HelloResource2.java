6453d1e428b0bc15d97f06d23186da2ab53f262f
Piotr Dasiak <piotr.dasiak@gmail.com>
2017-08-15 12:51:08 +0200

    commituje hasło

--- src/main/java/rest/HelloResource2.java
+++ src/main/java/rest/HelloResource2.java
@@ -1,14 +1,13 @@
 package main.java.rest;
 
 import java.util.Date;
-import java.util.Properties;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 @Path("/hello")
-public class HelloResource {
+public class HelloResource2 {
 
 	// @Inject
 	// private Logger logger;
@@ -16,10 +15,7 @@ public class HelloResource {
 	@GET
 	@Produces("text/plain")
 	public String helloResource() {
-
-		Properties properties = new Properties();
-		properties.put(REMOVED);
-
+		// logger.info("jhab");
 		return "Hello! It's " + new Date();
 	}
 }
