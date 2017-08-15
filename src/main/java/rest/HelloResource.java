608a69368406c0fed285ed6428fde9e3e0e198dd
Piotr Dasiak <piotr.dasiak@gmail.com>
2017-08-15 12:33:51 +0200

    Initial commit

---
+++ src/main/java/rest/HelloResource.java
@@ -0,0 +1,21 @@
+package main.java.rest;
+
+import java.util.Date;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/hello")
+public class HelloResource {
+
+	// @Inject
+	// private Logger logger;
+
+	@GET
+	@Produces("text/plain")
+	public String helloResource() {
+		// logger.info("jhab");
+		return "Hello! It's " + new Date();
+	}
+}
---
+++ src/main/java/rest/JaxRsActivator.java
@@ -0,0 +1,8 @@
+package main.java.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class JaxRsActivator extends Application {
+}
